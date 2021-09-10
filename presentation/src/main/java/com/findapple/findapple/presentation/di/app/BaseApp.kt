7e93bcc073d3df6f267adc68129e86640cb07ab9
Jaewon <turitus@naver.com>
2021-09-10 21:54:21 +0900

    [UPDATE]app ui

--- presentation/src/main/java/com/findapple/findapple/presentation/di/app/BaseApp.kt
+++ presentation/src/main/java/com/findapple/findapple/presentation/di/app/BaseApp.kt
@@ -1,14 +1,17 @@
 package com.findapple.findapple.presentation.di.app
 
+import com.findapple.findapple.R
 import com.findapple.findapple.presentation.di.component.DaggerAppComponent
+import com.kakao.sdk.common.KakaoSdk
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class BaseApp : DaggerApplication() {
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication>
-            = DaggerAppComponent.builder().application(this).build()
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
+        DaggerAppComponent.builder().application(this).build()
 
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, resources.getString(R.string.onlyKey))
     }
 }
