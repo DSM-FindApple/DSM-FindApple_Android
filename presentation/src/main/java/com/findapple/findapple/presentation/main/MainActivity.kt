2ebc5af2d492fdfb1b637da687c43aba953a16c5
Jaewon <turitus@naver.com>
2021-09-10 23:31:22 +0900

    [UPDATE]singleliveevent

--- presentation/src/main/java/com/findapple/findapple/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/findapple/findapple/presentation/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.findapple.findapple.presentation
+package com.findapple.findapple.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ presentation/src/main/java/com/findapple/findapple/presentation/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.findapple.findapple.presentation.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
