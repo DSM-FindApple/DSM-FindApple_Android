148bd81e365e8d5f41ef4d8c228d75b04575ee79
Jaewon <turitus@naver.com>
2021-09-23 22:28:09 +0900

    [CREATE] find get list

---
+++ presentation/src/main/java/com/findapple/findapple/presentation/features/find/viewmodel/FindViewModel.kt
@@ -0,0 +1,7 @@
+package com.findapple.findapple.presentation.features.find.viewmodel
+
+import com.findapple.findapple.domain.features.post.usecase.GetFindListUseCase
+import com.findapple.findapple.presentation.base.BaseViewModel
+
+class FindViewModel(private val getFindListUseCase: GetFindListUseCase) : BaseViewModel() {
+}
