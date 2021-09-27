788975c0f941a4d1ca996e97237732c1e4995505
Jaewon <turitus@naver.com>
2021-09-27 22:17:25 +0900

    [UPDATE]user repository

--- data/src/main/java/com/findapple/data/features/mypage/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/findapple/data/features/mypage/repository/UserRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package com.findapple.data.features.mypage.repository
 
+import com.findapple.data.features.mypage.datasource.UserDataSource
 import com.findapple.domain.features.mypage.entity.User
 import com.findapple.domain.features.mypage.repository.UserRepository
 import io.reactivex.Single
 
-class UserRepositoryImpl: UserRepository {
-    override fun getUserInfo(): Single<User> {
-        TODO("Not yet implemented")
-    }
+class UserRepositoryImpl(private val userDataSource: UserDataSource) : UserRepository {
+    override fun getUserInfo(): Single<User> =
+        userDataSource.getUserInfo()
 }
