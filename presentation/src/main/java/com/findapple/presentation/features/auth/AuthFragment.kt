2ec84e532609a94540b4583be23cc1fb43219bf4
Jaewon <turitus@naver.com>
2021-09-30 08:36:47 +0900

    [UPDATE] kakao login

--- presentation/src/main/java/com/findapple/presentation/features/auth/AuthFragment.kt
+++ presentation/src/main/java/com/findapple/presentation/features/auth/AuthFragment.kt
@@ -1,5 +1,8 @@
 package com.findapple.presentation.features.auth
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.findapple.presentation.R
@@ -9,14 +12,44 @@ import com.findapple.presentation.base.BaseViewModel
 import com.findapple.presentation.databinding.FragmentAuthBinding
 import com.findapple.presentation.features.auth.viewmodel.AuthViewModel
 import com.findapple.presentation.features.auth.viewmodel.AuthViewModelFactory
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.user.rx
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
 
 class AuthFragment : BaseFragment<FragmentAuthBinding>(R.layout.fragment_auth) {
 
     @Inject
     lateinit var viewModelFactory: AuthViewModelFactory
+
     override val viewModel: BaseViewModel by lazy {
         ViewModelProvider(this, viewModelFactory).get(AuthViewModel::class.java)
+    }
+
+    @Inject
+    lateinit var compositeDisposable: CompositeDisposable
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.authLoginBtn.setOnClickListener {
+            if(UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())){
+                UserApiClient.rx.loginWithKakaoTalk(requireContext())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+
+                    }, {
+
+                    })
+                    .addTo(compositeDisposable)
+            }else{
+                UserApiClient.instance.loginWithKakaoAccount(requireContext()) { token, error ->
+
+                }
+            }
+
+        }
     }
 
     override fun observeEvent() {
