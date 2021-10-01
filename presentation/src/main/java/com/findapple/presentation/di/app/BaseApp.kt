f7d19b9d0998391e4c3b6dd7906a66fe521991d3
Jaewon <turitus@naver.com>
2021-10-01 23:00:56 +0900

    [UPDATE] fcm build gradle

--- presentation/src/main/java/com/findapple/presentation/di/app/BaseApp.kt
+++ presentation/src/main/java/com/findapple/presentation/di/app/BaseApp.kt
@@ -1,14 +1,20 @@
 package com.findapple.presentation.di.app
 
+import com.findapple.data.local.sharedpref.LocalStorage
 import com.findapple.presentation.R
 import com.findapple.presentation.di.component.DaggerAppComponent
+import com.google.firebase.messaging.FirebaseMessaging
 import com.kakao.sdk.common.KakaoSdk
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import javax.inject.Inject
 
 class BaseApp : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.builder().application(this).build()
+
+    @Inject
+    lateinit var localStorage: LocalStorage
 
     override fun onCreate() {
         super.onCreate()
