a9d76e0a0717577585c5b216c46c66b40abf2b56
Jaewon <turitus@naver.com>
2021-10-07 11:58:32 +0900

    [UPDATE] login usecase

--- presentation/src/main/java/com/findapple/presentation/features/auth/AuthFragment.kt
+++ presentation/src/main/java/com/findapple/presentation/features/auth/AuthFragment.kt
@@ -1,18 +1,13 @@
 package com.findapple.presentation.features.auth
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import com.findapple.presentation.R
-import com.findapple.presentation.base.BackPressFinishFragment
 import com.findapple.presentation.base.BaseFragment
-import com.findapple.presentation.base.BaseViewModel
 import com.findapple.presentation.databinding.FragmentAuthBinding
 import com.findapple.presentation.features.auth.viewmodel.AuthViewModel
 import com.findapple.presentation.features.auth.viewmodel.AuthViewModelFactory
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
@@ -20,7 +15,6 @@ import com.kakao.sdk.user.rx
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
 
 class AuthFragment : BaseFragment<FragmentAuthBinding>(R.layout.fragment_auth) {
@@ -28,7 +22,7 @@ class AuthFragment : BaseFragment<FragmentAuthBinding>(R.layout.fragment_auth) {
     @Inject
     lateinit var viewModelFactory: AuthViewModelFactory
 
-    override val viewModel: BaseViewModel by lazy {
+    override val viewModel: AuthViewModel by lazy {
         ViewModelProvider(this, viewModelFactory).get(AuthViewModel::class.java)
     }
 
@@ -41,7 +35,7 @@ class AuthFragment : BaseFragment<FragmentAuthBinding>(R.layout.fragment_auth) {
         val afterLogin: DisposableSingleObserver<OAuthToken> =
             object : DisposableSingleObserver<OAuthToken>() {
                 override fun onSuccess(t: OAuthToken) {
-
+                    getUserInfo()
                 }
 
                 override fun onError(e: Throwable) {
@@ -68,10 +62,13 @@ class AuthFragment : BaseFragment<FragmentAuthBinding>(R.layout.fragment_auth) {
         }
     }
 
-    private fun getUserInfo(){
-        UserApiClient.instance.me { user, error ->
-            if(user != null){
-
+    private fun getUserInfo() {
+        UserApiClient.instance.me { user, _ ->
+            if (user != null) {
+                viewModel.login(user.id.toString(), user.kakaoAccount?.profile?.nickname.toString())
+                onBackPressed()
+            } else {
+                
             }
         }
     }
--- presentation/src/main/java/com/findapple/presentation/features/auth/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/findapple/presentation/features/auth/viewmodel/AuthViewModel.kt
@@ -1,14 +1,33 @@
 package com.findapple.presentation.features.auth.viewmodel
 
 import androidx.lifecycle.Lifecycle
+import com.findapple.domain.base.Result
+import com.findapple.domain.features.auth.entity.Auth
+import com.findapple.domain.features.auth.entity.Token
 import com.findapple.domain.features.auth.usecase.LoginUseCase
 import com.findapple.presentation.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
 
 class AuthViewModel(
-    private val LoginUseCase: LoginUseCase
+    private val loginUseCase: LoginUseCase
 ) : BaseViewModel() {
     override fun apply(event: Lifecycle.Event) {
     }
 
+    fun login(id: String, nickname: String) {
+        loginUseCase.execute(
+            Auth(id, nickname),
+            object : DisposableSingleObserver<Result<Token>>() {
+                override fun onSuccess(t: Result<Token>) {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            },
+            AndroidSchedulers.mainThread()
+        )
+    }
 
 }
