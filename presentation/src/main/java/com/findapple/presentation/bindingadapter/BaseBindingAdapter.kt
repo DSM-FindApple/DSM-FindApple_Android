28979b14fe69dd35d50a650569d400f617e18e4e
Jaewon <turitus@naver.com>
2021-10-19 10:27:44 +0900

    [UPDATE] find webview

---
+++ presentation/src/main/java/com/findapple/presentation/bindingadapter/BaseBindingAdapter.kt
@@ -0,0 +1,6 @@
+package com.findapple.presentation.bindingadapter
+
+import android.webkit.WebView
+import androidx.databinding.BindingAdapter
+
+
--- presentation/src/main/java/com/findapple/presentation/features/find/FindFragment.kt
+++ presentation/src/main/java/com/findapple/presentation/features/find/FindFragment.kt
@@ -1,5 +1,9 @@
 package com.findapple.presentation.features.find
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.findapple.presentation.R
 import com.findapple.presentation.databinding.FragmentFindBinding
@@ -14,6 +18,21 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
     lateinit var viewModelFactory: FindViewModelFactory
     override val viewModel: BaseViewModel by lazy {
         ViewModelProvider(this, viewModelFactory).get(FindViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setWebView()
+    }
+
+    private fun setWebView(){
+        binding.findWv.run {
+            settings.run {
+                javaScriptEnabled = true
+                builtInZoomControls = false
+            }
+            loadUrl("https://find-apple-client.vercel.app")
+        }
     }
 
     override fun observeEvent() {
