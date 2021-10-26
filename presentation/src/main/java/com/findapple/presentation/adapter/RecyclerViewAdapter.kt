eac3026a4238862e60c0cd11e0afb190a638999d
Jaewon <turitus@naver.com>
2021-10-26 17:18:30 +0900

    [CREATE] recyclerview adapter

---
+++ presentation/src/main/java/com/findapple/presentation/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,22 @@
+package com.findapple.presentation.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+}
