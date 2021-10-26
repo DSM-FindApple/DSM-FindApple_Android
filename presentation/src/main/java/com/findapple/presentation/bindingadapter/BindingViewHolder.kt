33b6088fe2b7ec107bec730c79a9fee17bc0688e
Jaewon <turitus@naver.com>
2021-10-26 18:49:11 +0900

    [UPDATE] recycler view items

---
+++ presentation/src/main/java/com/findapple/presentation/bindingadapter/BindingViewHolder.kt
@@ -0,0 +1,6 @@
+package com.findapple.presentation.bindingadapter
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class BindingViewHolder(val binding: ViewDataBinding): RecyclerView.ViewHolder(binding.root)
---
+++ presentation/src/main/java/com/findapple/presentation/bindingadapter/RecyclerViewItem.kt
@@ -0,0 +1,14 @@
+package com.findapple.presentation.bindingadapter
+
+import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
+
+data class RecyclerViewItem(
+    @LayoutRes val itemLayoutId: Int,
+    val data: Any,
+    val variableId: Int
+) {
+    fun bind(binding: ViewDataBinding) {
+        binding.setVariable(variableId, data)
+    }
+}
