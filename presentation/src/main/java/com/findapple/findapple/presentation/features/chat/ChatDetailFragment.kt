e267787110efbc733cdf61cc52b8c5134b54c6cd
Jaewon <turitus@naver.com>
2021-11-07 22:47:22 +0900

    [UPDATE] chat detail fragment

--- presentation/src/main/java/com/findapple/findapple/presentation/features/chat/ChatDetailFragment.kt
+++ presentation/src/main/java/com/findapple/findapple/presentation/features/chat/ChatDetailFragment.kt
@@ -1,15 +1,43 @@
 package com.findapple.findapple.presentation.features.chat
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.findapple.findapple.R
+import com.findapple.findapple.databinding.FragmentChatDetailBinding
 import com.findapple.findapple.databinding.FragmentChattingBinding
 import com.findapple.findapple.presentation.base.BaseFragment
 import com.findapple.findapple.presentation.base.BaseViewModel
+import com.findapple.findapple.presentation.features.chat.viewmodel.ChattingViewModel
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
-class ChatDetailFragment : BaseFragment<FragmentChattingBinding>(R.layout.fragment_chat_detail) {
-    override val viewModel: BaseViewModel
-        get() = TODO("Not yet implemented")
+class ChatDetailFragment : DaggerFragment() {
 
-    override fun observeEvent() {
+    lateinit var binding: FragmentChatDetailBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat_detail, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val idArg by navArgs<ChatDetailFragmentArgs>()
+        binding.url = "http://211.38.86.92:4046/chat/${idArg.chatRoomId}"
+
+        requireActivity().onBackPressedDispatcher.addCallback {
+            findNavController().navigateUp()
+        }
     }
 }
