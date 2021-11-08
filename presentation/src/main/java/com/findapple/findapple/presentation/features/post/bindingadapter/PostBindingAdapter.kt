39f5000526ea390bbb12005e968d309fe5e7f607
Jaewon <turitus@naver.com>
2021-11-08 11:14:25 +0900

    [UPDATE] date picker

--- presentation/src/main/java/com/findapple/findapple/presentation/features/post/bindingadapter/PostBindingAdapter.kt
+++ presentation/src/main/java/com/findapple/findapple/presentation/features/post/bindingadapter/PostBindingAdapter.kt
@@ -1,12 +1,14 @@
 package com.findapple.findapple.presentation.features.post.bindingadapter
 
 import android.net.Uri
+import android.widget.NumberPicker
 import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.findapple.findapple.presentation.features.post.adapter.PostPhotoAdapter
 import com.findapple.findapple.presentation.features.post.viewModel.PostViewModel
+import java.time.LocalDate
 
-@BindingAdapter("setPostPhotoItems", "viewModel")
+@BindingAdapter("setPostPhotoItems", "itemsViewModel")
 fun ViewPager2.setPostPhotoItems(
     images: List<Uri>?,
     viewModel: PostViewModel
@@ -16,5 +18,39 @@ fun ViewPager2.setPostPhotoItems(
     }
     if (images != null) {
         (adapter as? PostPhotoAdapter)?.updateData(images)
+    }
+}
+
+@BindingAdapter("setMaxValue")
+fun NumberPicker.setNumberPicker(
+    max: Int
+) {
+    wrapSelectorWheel = false
+
+    val now = LocalDate.now()
+    when (max) {
+        0 -> {
+            now.year.run {
+                maxValue = this
+                minValue = this - 1
+                value = this
+            }
+        }
+
+        12 -> {
+            now.month.value.run {
+                minValue = 1
+                maxValue = max
+                value = this
+            }
+        }
+
+        31 -> {
+            now.dayOfMonth.run {
+                minValue = 1
+                maxValue = max
+                value = this
+            }
+        }
     }
 }
