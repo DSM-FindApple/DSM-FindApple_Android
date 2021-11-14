60cff52e67989578bacf88b89e8eb191f1acaddb
Jaewon <turitus@naver.com>
2021-11-14 23:20:36 +0900

    [UPDATE] post data extension

--- domain/src/main/java/com/findapple/findapple/domain/features/post/parameter/PostDataParameter.kt
+++ domain/src/main/java/com/findapple/findapple/domain/features/post/parameter/PostDataParameter.kt
@@ -1,14 +1,13 @@
 package com.findapple.findapple.domain.features.post.parameter
 
+import android.net.Uri
 import com.findapple.findapple.domain.entity.Location
-import java.io.File
-import java.util.*
 
 data class PostDataParameter(
     val title: String,
     val detail: String,
     val category: String,
     val actionTime: String,
-    val images: List<File>,
+    val images: List<Uri>,
     val locationInfo: Location
 )
