f1ff4a86c81faaa6837e95521daeece3fa0b909e
Jaewon <turitus@naver.com>
2021-11-18 01:58:03 +0900

    [UPDATE] chat web bridge

--- app/src/main/java/com/findapple/findapple/app/features/chat/ChatWebBridge.kt
+++ app/src/main/java/com/findapple/findapple/app/features/chat/ChatWebBridge.kt
@@ -1,11 +1,17 @@
 package com.findapple.findapple.app.features.chat
 
 import android.webkit.JavascriptInterface
+import androidx.fragment.app.Fragment
 
-class ChatWebBridge(private val chattingFragment: ChattingFragment) {
+class ChatWebBridge(private val fragment: Fragment) {
 
     @JavascriptInterface
     fun startChatDetail(id: String) {
-        chattingFragment.moveToChattingDetail(id)
+        (fragment as ChattingFragment).moveToChattingDetail(id)
+    }
+
+    @JavascriptInterface
+    fun startSelectDate() {
+        (fragment as ChatDetailFragment).showDatePickerDialog()
     }
 }
