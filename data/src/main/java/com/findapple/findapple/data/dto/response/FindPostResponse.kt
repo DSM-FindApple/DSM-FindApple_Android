6ca376d76eda391e3df39e12e61e2e4ffbad3054
Jaewon <turitus@naver.com>
2021-11-27 14:00:16 +0900

    [UPDATE] find post response

---
+++ data/src/main/java/com/findapple/findapple/data/dto/response/FindPostResponse.kt
@@ -0,0 +1,38 @@
+package com.findapple.findapple.data.dto.response
+
+import com.findapple.findapple.domain.entity.Location
+import com.findapple.findapple.domain.entity.User
+import com.findapple.findapple.domain.features.post.entity.Post
+
+data class FindPostResponse (
+    val category: String,
+    val detail: String,
+    val findAt: String,
+    val findId: Long,
+    val findImages: List<String>,
+    val findUser: String,
+    val kakaoId: Long,
+    val latitude: Double,
+    val longitude: Double,
+    val profileUrl: String,
+    val title: String,
+    val topComment: TopCommentResponse,
+    val writeAt: String
+)
+
+fun List<FindPostResponse>.toEntity(): List<Post> =
+    this.map { it.toEntity() }
+
+fun FindPostResponse.toEntity(): Post {
+    return Post(
+        id = findId,
+        title = title,
+        user = User(name = findUser, profileImage = profileUrl, id = kakaoId),
+        detailInfo = detail,
+        actionTime = findAt,
+        writeTime = writeAt,
+        category = category,
+        location = Location(longitude, latitude),
+        images = findImages
+    )
+}
--- data/src/main/java/com/findapple/findapple/data/features/mypage/dto/response/UserResponse.kt
+++ data/src/main/java/com/findapple/findapple/data/features/mypage/dto/response/UserResponse.kt
@@ -1,7 +1,9 @@
 package com.findapple.findapple.data.features.mypage.dto.response
 
+import com.findapple.findapple.data.dto.response.FindPostResponse
 import com.findapple.findapple.data.dto.response.LostPostResponse
 import com.findapple.findapple.data.dto.response.toEntity
+import com.findapple.findapple.data.features.post.dto.response.FindListResponse
 import com.findapple.findapple.domain.features.mypage.entity.UserDetail
 
 data class UserResponse(
@@ -10,6 +12,7 @@ data class UserResponse(
     val kakaoId: Int,
     val findapplePoint: Int,
     val myLosts: List<LostPostResponse>,
+    val myFinds: List<FindPostResponse>,
     val nickName: String,
     val profileUrl: String
 )
