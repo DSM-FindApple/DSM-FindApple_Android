e85aa0ba0cff52951e3b6053a0f28727e337f539
Jaewon <turitus@naver.com>
2021-11-28 04:15:14 +0900

    [CREATE] comment viewmodel

---
+++ app/src/main/java/com/findapple/findapple/app/features/comment/viewmodel/CommentViewModel.kt
@@ -0,0 +1,10 @@
+package com.findapple.findapple.app.features.comment.viewmodel
+
+import androidx.lifecycle.Lifecycle
+import com.findapple.findapple.app.base.BaseViewModel
+
+class CommentViewModel: BaseViewModel() {
+    override fun apply(event: Lifecycle.Event) {
+        
+    }
+}
---
+++ app/src/main/java/com/findapple/findapple/app/features/comment/viewmodel/CommentViewModelFactory.kt
@@ -0,0 +1,10 @@
+package com.findapple.findapple.app.features.comment.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class CommentViewModelFactory: ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        TODO("Not yet implemented")
+    }
+}
